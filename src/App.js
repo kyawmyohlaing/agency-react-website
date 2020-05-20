23411d6a9748e644dc4bf1d6518301f23015ced9
kyawmyohlaing <kyawthanthat@gmail.com>
2020-05-20 15:40:10 +0630

    Add Home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
-import {router} 
-import PageWrapper from '../components/PageWrapper';
+
+import PageWrapper from './components/PageWrapper';
 
 class App extends Component {
 
   render(){
     return(
-      <div>
+      
           <PageWrapper />
-      </div>
+      
     )
   }
 }
