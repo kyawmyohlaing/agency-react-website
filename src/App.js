a300dab91e5a56fcf12e15c963919173348b63c8
kyawmyohlaing <kyawthanthat@gmail.com>
2020-05-21 10:21:41 +0630

    Add Team Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PageWrapper from './components/PageWrapper';
 import Home from './pages/Home';
 import About from './pages/About';
+import Team from './pages/Team';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 class App extends Component {
@@ -20,6 +21,11 @@ class App extends Component {
            
            path="/about"
            component={About}
+        />
+        <Route 
+           
+           path="/team"
+           component={Team}
         />
          
            
---
+++ src/pages/Team.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+
+class Team extends Component{
+
+    render(){
+
+        return(
+            <div>
+                <section className="page-section bg-light" id="team">
+                        <div className="container">
+                                <div className="text-center">
+                                    <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
+                                    <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                                </div>
+
+                                <div className="row">
+                                    <div className="col-lg-4">
+                                        <div className="team-member">
+                                            <img className="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="" />
+                                            <h4>Kay Garland</h4>
+                                            <p className="text-muted">Lead Designer</p>
+                                            <Link className="btn btn-dark btn-social mx-2" to="#!">
+                                                <i className="fab fa-twitter"></i>
+                                            </Link>
+                                            <Link className="btn btn-dark btn-social mx-2" to="#!">
+                                                <i className="fab fa-facebook-f"></i>
+                                            </Link>
+                                            <Link className="btn btn-dark btn-social mx-2" to="#!">
+                                                <i className="fab fa-linkedin-in"></i>
+                                            </Link>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-4">
+                                        <div className="team-member">
+                                            <img className="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="" />
+                                            <h4>Larry Parker</h4>
+                                            <p className="text-muted">Lead Marketer</p>
+                                            <Link className="btn btn-dark btn-social mx-2" to="#!">
+                                                <i className="fab fa-twitter"></i>
+                                            </Link>
+                                            <Link className="btn btn-dark btn-social mx-2" to="#!">
+                                                <i className="fab fa-facebook-f"></i>
+                                            </Link>
+                                            <Link className="btn btn-dark btn-social mx-2" to="#!">
+                                                <i className="fab fa-linkedin-in"></i>
+                                            </Link>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-4">
+                                        <div className="team-member">
+                                            <img className="mx-auto rounded-circle" src="assets/img/team/3.jpg" alt="" />
+                                            <h4>Diana Petersen</h4>
+                                            <p className="text-muted">Lead Developer</p>
+                                            <Link className="btn btn-dark btn-social mx-2" to="#!">
+                                                <i className="fab fa-twitter"></i>
+                                            </Link>
+                                            <Link className="btn btn-dark btn-social mx-2" to="#!">
+                                                <i className="fab fa-facebook-f"></i>
+                                            </Link>
+                                            <Link className="btn btn-dark btn-social mx-2" to="#!">
+                                                <i className="fab fa-linkedin-in"></i>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+
+                                <div className="row">
+                                    <div className="col-lg-8 mx-auto text-center">
+                                        <p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+                                    </div>
+                                </div>
+                        </div>          
+                </section>
+        </div>)
+    }
+}
+export default Team;
